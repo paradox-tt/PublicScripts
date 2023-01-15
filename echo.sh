0dee055783edcd5fc29ca4c223f79dba11080027
Paradox <support@paranodes.io>
2023-01-15 00:56:26 +0000

    Removed Spaces

--- echo.sh
+++ echo.sh
@@ -4,9 +4,9 @@ latest_version=$(echo $releases | jq -r '.tag_name');
 latest_file=$(echo $releases | jq -r '.assets[] | select(.name == "polkadot") | .browser_download_url');
 latest_filesha=$(echo $releases | jq -r '.assets[] | select(.name == "polkadot.sha256") | .browser_download_url');
 
-prometheus_port = 9700;
+prometheus_port=9700;
 
-instance_version = $(curl -s http://localhost:$prometheus_port/metrics | grep substrate_build_info{ | awk -F ' ' '{ print $1 }' | awk -F { '{ print "{"$2}' | awk -F version=\" '{print $2}' | awk -F \" '{print $1}');
+instance_version=$(curl -s http://localhost:$prometheus_port/metrics | grep substrate_build_info{ | awk -F ' ' '{ print $1 }' | awk -F { '{ print "{"$2}' | awk -F version=\" '{print $2}' | awk -F \" '{print $1}');
 
 echo "Current binary version : $latest_version";
 echo "Current instance version : $instance_version";
