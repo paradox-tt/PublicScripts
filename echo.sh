6b3645ad7b7a6a0a67919832ff200f80dba1674f
Paradox <support@paranodes.io>
2023-01-15 02:18:56 +0000

    Removing v

--- echo.sh
+++ echo.sh
@@ -2,7 +2,7 @@
 download_path="\home\ansible_user";
 
 releases=$(curl -s https://api.github.com/repos/paritytech/polkadot/releases/latest);
-latest_version=$(echo $releases | jq -r '.tag_name');
+latest_version=$(echo $releases | jq -r '.tag_name' | awk -F "v" '{print $2}');
 latest_file=$(echo $releases | jq -r '.assets[] | select(.name == "polkadot") | .browser_download_url');
 latest_filesha=$(echo $releases | jq -r '.assets[] | select(.name == "polkadot.sha256") | .browser_download_url');
 
@@ -11,7 +11,7 @@ current_release=$(polkadot --version | awk '{print $2}' | awk -F "-" '{print $1}
 prometheus_port=$1;
 service_file=$2;
 
-instance_version=$(curl -s http://localhost:$prometheus_port/metrics | grep substrate_build_info{ | awk -F ' ' '{ print $1 }' | awk -F { '{ print "{"$2}' | awk -F version=\" '{print $2}' | awk -F "-" '{print "v"$1}');
+instance_version=$(curl -s http://localhost:$prometheus_port/metrics | grep substrate_build_info{ | awk -F ' ' '{ print $1 }' | awk -F { '{ print "{"$2}' | awk -F version=\" '{print $2}' | awk -F "-" '{print $1}');
 is_parachain_validator=$(curl -s http://localhost:$prometheus_port/metrics | grep polkadot_node_is_parachain_validator{ | awk '{print $2}');
 
 #Show variables attained
