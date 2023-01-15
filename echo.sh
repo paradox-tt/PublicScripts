8d820e9556eca1104a771b7bbbf1ba84beb7b573
Paradox <support@paranodes.io>
2023-01-15 02:52:59 +0000

    Added override for third party download

--- echo.sh
+++ echo.sh
@@ -9,14 +9,20 @@ latest_filesha=$(echo $releases | jq -r '.assets[] | select(.name == "polkadot.s
 current_release=$(polkadot --version | awk '{print $2}' | awk -F "-" '{print $1}');
 
 prometheus_port=$1;
-service_file=$2;
+service=$2;
+override_download=$3;
+
+if [ override_download!="" ]
+then
+    latest_file=override_download;
+fi
 
 instance_version=$(curl -s http://localhost:$prometheus_port/metrics | grep substrate_build_info{ | awk -F ' ' '{ print $1 }' | awk -F { '{ print "{"$2}' | awk -F version=\" '{print $2}' | awk -F "-" '{print $1}');
 is_parachain_validator=$(curl -s http://localhost:$prometheus_port/metrics | grep polkadot_node_is_parachain_validator{ | awk '{print $2}');
 
 #Show variables attained
 echo "Prometheus port : $prometheus_port";
-echo "Service filename : $service_file";
+echo "Service filename : $service";
 echo "Download path: $download_path";
 
 echo "Latest binary version : $latest_version";
@@ -31,4 +37,13 @@ then
     echo "Downloading $latest_file";
 
     $(curl -sL $latest_file -o $download_path"/polkadot");
+    
+    echo "Moving file to /usr/local/bin";
+fi
+
+if [ $is_parachain_validator != 1 && $instance_version != $current_release]
+then
+    echo "Daemon reloading and restarting service";
+    $(sudo systemctl daemon-reload);
+    $(sudo systemctl restart $service);
 fi
